d37017fe5916c0dcdf333ebd377bbd454159c85c
Sobir777 <84676914+Sobir777@users.noreply.github.com>
2022-02-20 16:49:28 +0500

    changes made

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,7 @@ import React from 'react'
 import Heder from './Heder/index.js'
 import Footer from './Footer/index.js'
 
-
-
 export default function Layout({ children }) {
-
-
     return (
         <>
             <Heder />
